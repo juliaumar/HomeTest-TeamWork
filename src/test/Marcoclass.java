7b6057d9a3706bd92527eec528e8b4aaed434910
Davor Pavlovic <davorskola21@gmail.com>
2022-05-30 20:28:35 -0400

    practiceJava

---
+++ src/test/Marcoclass.java
@@ -0,0 +1,21 @@
+public class Marcoclass {
+    public static void main(String[] args) {
+
+
+        String name = "Davor";
+
+        String re = "";
+
+        for (int i =name.length()-1;i>=0;i-- ) {
+
+            re += name.charAt(i);
+            System.out.println(re);
+        }
+
+
+    }
+}
+
+
+
+
